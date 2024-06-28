a72bbda3e6ac36806c23a5c7e88a8943d6bab60b
Martin Roy <martin@boostsecurity.io>
2024-06-28 13:25:16 -0400

    Add flask app

--- Dockerfile
+++ Dockerfile
@@ -3,3 +3,8 @@ FROM python:3
 
 RUN pip install "requests"
 RUN pip install "pillow==8.4.0"
+RUN pip install "flask"
+
+COPY mbd2_test_repo/api.py /
+
+CMD flask --app api run --host 0.0.0.0
---
+++ build.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env sh
+
+set -eux
+
+docker build --tag lindycoder/dev-test-repo:latest --label "org.opencontainers.image.source=https://github.com/martin-boost-dev-2/test-repo" .
+docker push lindycoder/dev-test-repo:latest
