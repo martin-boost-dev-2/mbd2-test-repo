bfcb72d217fa9298857a60a3dc0efa8ac8b87b18
Martin Roy <martin@boostsecurity.io>
2024-08-05 15:17:34 -0400

    Add framework and saas

--- build.sh
+++ build.sh
@@ -2,5 +2,6 @@
 
 set -eux
 
-docker build --tag lindycoder/dev-test-repo:latest --label "org.opencontainers.image.source=https://github.com/martin-boost-dev-2/test-repo" .
+docker build --tag lindycoder/dev-test-repo:latest .
+#docker build --tag lindycoder/dev-test-repo:latest --label "org.opencontainers.image.source=https://github.com/martin-boost-dev-2/test-repo" .
 docker push lindycoder/dev-test-repo:latest
